184236b7c8bc9c2bb9ecb9f68c7932bdd2db644a
Sigi3012 <50233886+Sigi3012@users.noreply.github.com>
2024-07-17 15:35:52 +0100

    Refactors link fixer

--- backend/src/links.rs
+++ backend/src/links.rs
@@ -1,29 +1,28 @@
 use fancy_regex::Regex;
 use log::{debug, error, info, warn};
 use serde::Deserialize;
-use std::fs;
 
 lazy_static! {
     #[derive(Debug)]
-    static ref BUILT_PATTERNS: Vec<(Regex, String)> = build_all().unwrap();
+    static ref BUILT_PATTERNS: Vec<BuiltPattern> = build_all().unwrap();
 }
 
 #[derive(Deserialize)]
-struct PatternReplacement {
+struct LoadedJson {
     pattern: String,
     replacement: String,
 }
 
-fn load_json_patterns() -> Result<Vec<(String, String)>, Box<dyn std::error::Error>> {
-    let file = fs::read_to_string("patterns.json")?;
-    let deserialized: Vec<PatternReplacement> = serde_json::from_str(&file)?;
+struct BuiltPattern {
+    pattern: Regex,
+    replacement: String,
+}
 
-    let vec_tuples: Vec<(String, String)> = deserialized
-        .into_iter()
-        .map(|pr| (pr.pattern, pr.replacement))
-        .collect();
+fn load_json_patterns() -> Result<Vec<LoadedJson>, Box<dyn std::error::Error>> {
+    let file = include_str!("../../patterns.json");
+    let deserialized: Vec<LoadedJson> = serde_json::from_str(&file)?;
 
-    Ok(vec_tuples)
+    Ok(deserialized)
 }
 
 fn build_regex(pattern: &str) -> Result<Regex, fancy_regex::Error> {
@@ -36,16 +35,17 @@ fn build_regex(pattern: &str) -> Result<Regex, fancy_regex::Error> {
     }
 }
 
-fn build_all() -> Result<Vec<(Regex, String)>, Box<dyn std::error::Error>> {
-    let mut patterns: Vec<(Regex, String)> = Vec::new();
+fn build_all() -> Result<Vec<BuiltPattern>, Box<dyn std::error::Error>> {
+    let mut patterns: Vec<BuiltPattern> = Vec::new();
 
     match load_json_patterns() {
-        Ok(result) => {
-            // [(p, r), (p, r), ..]
-            for (p, r) in result.iter() {
-                let regex_pattern = build_regex(&p)?;
-                let regex_replacement = r.clone();
-                patterns.push((regex_pattern, regex_replacement))
+        Ok(jsons) => {
+            for item in jsons.iter() {
+                let regex_pattern = build_regex(&item.pattern)?;
+                patterns.push(BuiltPattern {
+                    pattern: regex_pattern,
+                    replacement: item.replacement.to_owned(),
+                });
             }
         }
         Err(e) => {
@@ -63,16 +63,14 @@ pub async fn fix_links(
     message: &poise::serenity_prelude::Message,
 ) -> Result<Option<String>, fancy_regex::Error> {
     let mut result = message.content.clone();
-    // Check if a message contains a link within patterns.json patterns
-    for p in BUILT_PATTERNS.iter() {
-        match p.0.is_match(&message.content) {
-            Ok(forwarded) => {
-                if forwarded == true {
-                    result = p.0.replace_all(&result, &p.1).to_string();
-                    debug!("{}", result)
-                }
-            }
-            Err(e) => warn!("{}", e),
+    // Check if a message contains a link within the loaded patterns
+    for built in BUILT_PATTERNS.iter() {
+        if built.pattern.is_match(&message.content)? == true {
+            result = built
+                .pattern
+                .replace_all(&result, &built.replacement)
+                .to_string();
+            debug!("{}", result)
         }
     }
 
